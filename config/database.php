f833c7cedc1df3df2b722014042b3e62fd91c9ef
Amandarisma <l200220119@student.ums.ac.id>
2026-04-11 11:34:11 +0700

    hapus terkait vercel, vercel.json, web.php, config/database.php

--- config/database.php
+++ config/database.php
@@ -58,9 +58,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => env('APP_ENV') === 'production' ? [
-    PDO::MYSQL_ATTR_SSL_CA => '/etc/pki/tls/certs/ca-bundle.crt',
-] : [],
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+]) : [],
             // 'options' => extension_loaded('pdo_mysql') ? array_filter([
             //     PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             // ]) : [],
